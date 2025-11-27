d4501ecd38f6ceb85a807646cc40c565aa6bfcf2
Hemanth04-gif <panakalupanakalu7@gmail.com>
2025-11-27 13:04:15 +0530

    Create 04_measures_exploration.sql

---
+++ scripts/04_measures_exploration.sql
@@ -0,0 +1,12 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
