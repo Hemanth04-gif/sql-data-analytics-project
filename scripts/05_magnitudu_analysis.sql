b7d39903746a405564e5d95eba28a441edc69989
Hemanth04-gif <panakalupanakalu7@gmail.com>
2025-11-27 16:48:43 +0530

    Create 05_magnitudu_analysis.sql

---
+++ scripts/05_magnitudu_analysis.sql
@@ -0,0 +1,13 @@
+/*
+===============================================================================
+Magnitude Analysis
+===============================================================================
+Purpose:
+    - To quantify data and group results by specific dimensions.
+    - For understanding data distribution across categories.
+
+SQL Functions Used:
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+    - GROUP BY, ORDER BY
+===============================================================================
+*/
