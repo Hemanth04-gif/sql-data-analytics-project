a233d0639c56e5e6fa94ccf10c62bb89daa979b0
Hemanth04-gif <panakalupanakalu7@gmail.com>
2025-11-28 09:56:27 +0530

    Update 07_change_over_time_analysis.sql

--- scripts/07_change_over_time_analysis.sql
+++ scripts/07_change_over_time_analysis.sql
@@ -36,3 +36,14 @@ FROM gold.fact_sales
 WHERE order_date IS	NOT NULL
 GROUP BY DATETRUNC(month,order_date)
 ORDER BY DATETRUNC(month,order_date);
+
+-- FORMAT()
+SELECT 
+	FORMAT(order_date,'yyyy-MMM') AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS	NOT NULL
+GROUP BY FORMAT(order_date,'yyyy-MMM')
+ORDER BY FORMAT(order_date,'yyyy-MMM');
